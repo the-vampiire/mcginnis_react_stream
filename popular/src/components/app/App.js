d96fa496b8bade73af5ef717de0a4953da56def3
Vampiire <vampiire@Vamps-MacBook-Pro.local>
2018-04-21 15:36:42 -0400

    base foundation for Popular Repos application

---
+++ popular/src/components/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { SSL_OP_PKCS1_CHECK_1 } from 'constants';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ popular/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+
+    module: {
+        rules: [
+            // one rule for each file type
+            {
+                // filetype to be transpiled
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    // the transpiling tool used for this filetype
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
