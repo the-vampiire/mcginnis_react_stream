bd17dbee3f9f8e59cc06a7556d05f2a20a833463
Vampiire <vampiire@Vamps-MacBook-Pro.local>
2018-04-23 03:03:54 -0400

    fixed webpack config to recognize jsx extensions

--- popular/webpack.config.js
+++ popular/webpack.config.js
@@ -2,37 +2,42 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+  entry: './src/index.jsx',
 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
 
-    module: {
-        rules: [
-            // one rule for each file type
-            {
-                // filetype to be transpiled
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    // the transpiling tool used for this filetype
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                // filetype to be transpiled
-                test: /\.css$/,
+  // required to recognize js and jsx files
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      // one rule for each file type
+      {
+        // filetype to be transpiled
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          // the transpiling tool used for this filetype
+          loader: 'babel-loader',
+        },
+      },
+      {
+        // filetype to be transpiled
+        test: /\.css$/,
         // **** STYLE LOADER MUST COME FIRST ****
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
